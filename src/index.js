0e64b85b752b142e9844a027dd8636773270c372
Candle779 <candle77779@gmail.com>
2020-10-01 09:32:02 +0300

    fix: change index.js to solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array != undefined && array.length > 0) {
+  var len = array.length, min = Infinity;
+  while (len--) {
+    if (array[len] < min) {
+      min = array[len];
+    }
+  }
+
+  return min;
+  
+}
+else {return 0;}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array != undefined && array.length > 0) {
+  var len = array.length, max = -Infinity;
+  while (len--) {
+    if (array[len] > max) {
+      max = array[len];
+    }
+  }
+  return max;}
+  else {return 0;}
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  
+  if (array != undefined && array.length > 0) {
+     return array.reduce((a,b) => (a + b)) /array.length;
+    }
+  else {return 0;}
+  
+  }
+ 
